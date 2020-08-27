382d599cf953acc82f544605ba09a84f8dfe9bc4
daPoseidonGuy <pelletiertenzin2000@gmail.com>
2020-08-27 11:04:19 -0400

    Added custom golem textures

--- src/main/java/com/commodorethrawn/strawgolem/client/renderer/entity/RenderStrawGolem.java
+++ src/main/java/com/commodorethrawn/strawgolem/client/renderer/entity/RenderStrawGolem.java
@@ -12,12 +12,43 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
 
 import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RenderStrawGolem extends MobRenderer<EntityStrawGolem, ModelStrawGolem<EntityStrawGolem>> {
 
-    private static final ResourceLocation TEXTURE = new ResourceLocation(Strawgolem.MODID, "textures/entity/straw_golem.png");
-    private static final ResourceLocation TEXTURE_OLD = new ResourceLocation(Strawgolem.MODID, "textures/entity/old_straw_golem.png");
-    private static final ResourceLocation TEXTURE_DYING = new ResourceLocation(Strawgolem.MODID, "textures/entity/dying_straw_golem.png");
+    private static final Map<String, ResourceLocation> TEXTURE_MAP;
+    private static final String TEXTURE_DEFAULT, TEXTURE_OLD, TEXTURE_DYING, TEXTURE_WINTER;
+    private static final boolean IS_DECEMBER;
+
+    static {
+        TEXTURE_DEFAULT = "golem";
+        TEXTURE_OLD = "old_golem";
+        TEXTURE_DYING = "dying_golem";
+        TEXTURE_WINTER = "winter_golem";
+        TEXTURE_MAP = new HashMap<>();
+        InputStream stream = Strawgolem.class.getResourceAsStream("/assets/strawgolem/textures/entity");
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+            while (reader.ready()) {
+                String name = reader.readLine();
+                if (name != null) name = name.replace(".png", "");
+                TEXTURE_MAP.put(name, new ResourceLocation(Strawgolem.MODID, "textures/entity/" + name + ".png"));
+            }
+            stream.close();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        IS_DECEMBER = GregorianCalendar.getInstance().get(Calendar.MONTH) == Calendar.DECEMBER;
+    }
 
     public RenderStrawGolem(EntityRendererManager rendermanagerIn) {
         super(rendermanagerIn, new ModelStrawGolem<>(), 0.5f);
@@ -43,12 +74,18 @@ public class RenderStrawGolem extends MobRenderer<EntityStrawGolem, ModelStrawGo
     @Nonnull
     @Override
     public ResourceLocation getEntityTexture(EntityStrawGolem golem) {
-        if (golem.getCurrentLifespan()  * 4 < ConfigHelper.getLifespan()) {
-            return TEXTURE_DYING;
-        } else if (golem.getCurrentLifespan() * 2 < ConfigHelper.getLifespan()) {
-            return TEXTURE_OLD;
-        }
-        return TEXTURE;
+        int lifespan = golem.getCurrentLifespan();
+        int maxLifespan = ConfigHelper.getLifespan();
+        if (lifespan * 4 < maxLifespan) return TEXTURE_MAP.get(TEXTURE_DYING);
+        String name = golem.getDisplayName().getString().toLowerCase();
+        if (TEXTURE_MAP.containsKey(name)) return TEXTURE_MAP.get(name);
+        if (IS_DECEMBER) return TEXTURE_MAP.get(TEXTURE_WINTER);
+        return lifespan * 2 < maxLifespan ? TEXTURE_MAP.get(TEXTURE_OLD) : TEXTURE_MAP.get(TEXTURE_DEFAULT);
     }
 
+    @Override
+    public void renderName(EntityStrawGolem entity, double x, double y, double z) {
+        if (!TEXTURE_MAP.containsKey(entity.getDisplayName().getString().toLowerCase()))
+            super.renderName(entity, x, y, z);
+    }
 }
